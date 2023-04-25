d0e25dac0467ae0b1944f9b9a4b4b7784a777da5
jiekeller <jkeller1@macalester.edu>
2023-04-24 19:48:13 -0500

    indeed changes have been made to the instructions scene

--- src/scenes/InstructionsScene.js
+++ src/scenes/InstructionsScene.js
@@ -9,19 +9,45 @@ class EndScene extends BaseScene{
     create(){
         this.createBackground();
         this.createInstructions();
-        this.nameLabel = this.add.text(285, 120, 'How to Play!',{
-            fontSize:"40px",
-            fill: "#E3E3E3",
-            fontFamily: "'8BIT WONDER', sans-serif",
-        }).setStroke("#0E0E0E", 10);
+        this.createLabel();
 
-        this.startInstruction = this.add.text(230, 530, 'Press SPACE to continue!', {
+        this.startInstruction = this.add.text(200, 530, 'Press SPACE to continue!', {
             fontSize:"30px",
             fill: "#E3E3E3",
             fontFamily: "'8BIT WONDER', sans-serif",
         }).setStroke("#0E0E0E", 10);
         this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);   
 
+    }
+
+    createLabel() {
+        this.firstColor = Phaser.Display.Color.HexStringToColor("#FFFFFF");
+        this.secondColor = Phaser.Display.Color.HexStringToColor("#0E0E0E");
+
+        this.label = this.add.text(323, 120, "How to Play!")
+            .setFontSize(35)
+            .setColor("#E3E3E3")
+            .setStroke("#0E0E0E", 10)
+            .setFontFamily("'8BIT WONDER', sans-serif");
+
+        this.tweens.addCounter({
+            from: 0,
+            to: 100,
+            duration: 500,
+            yoyo: true,
+            repeat: -1,
+            ease: Phaser.Math.Easing.Sine.InOut,
+            onUpdate: (tween) => {
+                let value = tween.getValue()
+                let color = Phaser.Display.Color.Interpolate.ColorWithColor(
+                    this.firstColor,
+                    this.secondColor,
+                    100,
+                    value
+                )
+                this.label.setTint(Phaser.Display.Color.GetColor(color.r, color.g, color.b));
+            }
+        });
     }
 
         
